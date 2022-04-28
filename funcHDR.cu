348702e092a1c6e6127012ce58134d8ae7e9b46a
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2022-04-28 17:59:43 +0200

    Add files via upload

--- funcHDR.cu
+++ funcHDR.cu
@@ -4,118 +4,60 @@
 #include <iostream>
 #include <iomanip>
 
-__constant__ float d_min, d_range;
-__constant__ int SIZE;
+//__constant__ float d_min, d_range;
+
+
+
 float* d_auxLuminanceForMin;
 float* d_auxLuminanceForMax;
 unsigned int* d_histogram;
 
-void calculate_cdf(const float* const d_logLuminance,
-                                  unsigned int* const d_cdf,
-                                  float &min_logLum,
-                                  float &max_logLum,
-                                  const size_t numRows,
-                                  const size_t numCols,
-                                  const size_t numBins)
-{
-    const dim3 blockSize(32, 32, 1);
-    const dim3 gridSize((numCols - 1) / 32 + 1, (numRows - 1) / 32 + 1, 1);
 
-  /* TODO
-    1) Encontrar el valor máximo y mínimo de luminancia en min_logLum and max_logLum a partir del canal logLuminance 
-
-    */
-    
-    
-    int size = numRows * numCols;
-
-    cudaMalloc(&d_auxLuminanceForMax, numRows*numCols*sizeof(float));
-    cudaMalloc(&d_auxLuminanceForMin, numRows * numCols * sizeof(float));
-    cudaMalloc(&d_histogram, numRows * numCols * sizeof(float));
-
-    cudaMemcpy(d_auxLuminanceForMax, d_logLuminance, numRows * numCols * sizeof(float), cudaMemcpyDeviceToDevice);
-    cudaMemcpy(d_auxLuminanceForMin, d_logLuminance, numRows * numCols * sizeof(float), cudaMemcpyDeviceToDevice);
-    
-
-    //llamada al kernel
-    maxAndMinReduce <<<gridSize,blockSize >>> (min_logLum, max_logLum, d_auxLuminanceForMin, d_auxLuminanceForMax, size);
-    //
-    cudaMemcpy(&min_logLum, d_auxLuminanceForMin, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaMemcpy(&max_logLum, d_auxLuminanceForMax, sizeof(float), cudaMemcpyDeviceToHost);
-
-    cudaMemcpyToSymbol(&d_min, d_auxLuminanceForMin, sizeof(float),cudaMemcpyDeviceToDevice);
-
-   /* cudaFree(d_auxLuminanceForMax);
-    cudaFree(d_auxLuminanceForMin);*/
-
-    float range = max_logLum - max_logLum;
-
-    cudaMemcpyToSymbol(&d_range,&range , sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpyToSymbol(&SIZE, &size, sizeof(int), cudaMemcpyHostToDevice);
-
-    histogram <<<gridSize, blockSize >>> (d_logLuminance, d_histogram, size, numBins);
-
-    exclusiveScan <<<gridSize, blockSize >>> (d_histogram, size);
-
-    cudaMemcpy(d_cdf, d_histogram, sizeof(unsigned int) * size, cudaMemcpyDeviceToDevice);
-    /*
-	2) Obtener el rango a representar
-	3) Generar un histograma de todos los valores del canal logLuminance usando la formula 
-	bin = (Lum [i] - lumMin) / lumRange * numBins
-	4) Realizar un exclusive scan en el histograma para obtener la distribución acumulada (cdf) 
-	de los valores de luminancia. Se debe almacenar en el puntero c_cdf
-  */    
-}
-
-
-__global__ void maxAndMinReduce(float &min_logLum, float &max_logLum, float* d_auxLuminanceForMin, float* d_auxLuminanceForMax, int len) {
+__global__ void maxAndMinReduce(const float* const d_logLuminance,float& min_logLum, float& max_logLum, float* d_auxLuminanceForMin, float* d_auxLuminanceForMax, int len) {
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     // RELLENAR CON EL VALOR IDENTIDAD
     if (i >= len) {
         d_auxLuminanceForMin[threadIdx.x] = 0.f;
+        d_auxLuminanceForMax[threadIdx.x] = 0.f;
     }
     else {
-        d_auxLuminanceForMin[threadIdx.x] = d_auxLuminanceForMin[i];
+        d_auxLuminanceForMin[threadIdx.x] = d_logLuminance[i];
+        d_auxLuminanceForMax[threadIdx.x] = d_logLuminance[i];
     }
     __syncthreads();
 
     for (unsigned int s = blockDim.x / 2; s > 0; s /= 2) {
         if (i < s) {
-            if (d_auxLuminanceForMin[threadIdx.x] <= d_auxLuminanceForMin[threadIdx.x + s]) {
-                d_auxLuminanceForMin[threadIdx.x] = d_auxLuminanceForMin[threadIdx.x];
-            }
-            else {
+            
+            if (d_auxLuminanceForMin[threadIdx.x] > d_auxLuminanceForMin[threadIdx.x + s]) {
                 d_auxLuminanceForMin[threadIdx.x] = d_auxLuminanceForMin[threadIdx.x + s];
             }
 
             if (d_auxLuminanceForMax[threadIdx.x] <= d_auxLuminanceForMax[threadIdx.x + s]) {
-                
+
                 d_auxLuminanceForMax[threadIdx.x] = d_auxLuminanceForMax[threadIdx.x + s];
             }
-            else {
-                d_auxLuminanceForMax[threadIdx.x] = d_auxLuminanceForMax[threadIdx.x];
-            }
-            
+
         }
         __syncthreads();
     }
 }
 
-__global__ void histogram(float* d_logLuminance,unsigned int* histo ,int len, int numBins) {
+__global__ void histogram(const float* const d_logLuminance, unsigned int* histo, int len, int numBins,float min, float range) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int bin;
     // All threads handle blockDim.x * gridDim.x consecutive elements
     if (i < len) {
-        bin = (d_logLuminance[i] - d_min) / d_range * numBins;
+        bin = (d_logLuminance[i] - min) / range * numBins;
         atomicAdd(&(histo[bin]), 1); //Varios threads podrían intentar incrementar el mismo valor a la vez
     }
 }
 
 
-__global__ void exclusiveScan(float* histo, int len) {
-    
-    __shared__ float tempArray[SIZE*2 ];
+__global__ void exclusiveScan(unsigned int* histo, int len) {
+
+    extern  __shared__ float tempArray[];
     int id = blockIdx.x * blockDim.x + threadIdx.x;
     int threadId = threadIdx.x;
     int offset = 1, temp;
@@ -159,3 +101,63 @@ __global__ void exclusiveScan(float* histo, int len) {
     histo[id + len / 2] = tempArray[threadId + len / 2];
 
 }
+
+
+
+void calculate_cdf(const float* const d_logLuminance,
+                                  unsigned int* const d_cdf,
+                                  float &min_logLum,
+                                  float &max_logLum,
+                                  const size_t numRows,
+                                  const size_t numCols,
+                                  const size_t numBins)
+{
+    const dim3 blockSize(32, 32, 1);
+    const dim3 gridSize((numCols - 1) / 32 + 1, (numRows - 1) / 32 + 1, 1);
+
+  /* TODO
+    1) Encontrar el valor máximo y mínimo de luminancia en min_logLum and max_logLum a partir del canal logLuminance 
+
+    */
+    
+    
+    int size = numRows * numCols;
+
+    cudaMalloc(&d_auxLuminanceForMax,size*sizeof(float));
+    cudaMalloc(&d_auxLuminanceForMin, size * sizeof(float));
+    cudaMalloc(&d_histogram, numBins* sizeof(unsigned int));
+    
+
+    //llamada al kernel
+    maxAndMinReduce <<<gridSize,blockSize >>> (d_logLuminance,min_logLum, max_logLum, d_auxLuminanceForMin, d_auxLuminanceForMax, size);
+    //
+
+
+    cudaMemcpy(&min_logLum, d_auxLuminanceForMin, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&max_logLum, d_auxLuminanceForMax, sizeof(float), cudaMemcpyDeviceToHost);
+
+    //cudaMemcpyToSymbol(&d_min, d_auxLuminanceForMin, sizeof(float),cudaMemcpyDeviceToDevice);
+
+   /* cudaFree(d_auxLuminanceForMax);
+    cudaFree(d_auxLuminanceForMin);*/
+
+    float range = max_logLum - min_logLum;
+    
+
+   // cudaMemcpyToSymbol(&d_range,&range , sizeof(float), cudaMemcpyHostToDevice);
+
+    histogram <<<gridSize, blockSize >>> (d_logLuminance, d_histogram, size, numBins,min_logLum,range);
+
+    exclusiveScan <<<gridSize, blockSize,numBins*2 >>> (d_histogram, numBins);
+
+    cudaMemcpy(d_cdf, d_histogram, sizeof( unsigned int) * numBins, cudaMemcpyDeviceToDevice);
+    /*
+	2) Obtener el rango a representar
+	3) Generar un histograma de todos los valores del canal logLuminance usando la formula 
+	bin = (Lum [i] - lumMin) / lumRange * numBins
+	4) Realizar un exclusive scan en el histograma para obtener la distribución acumulada (cdf) 
+	de los valores de luminancia. Se debe almacenar en el puntero c_cdf
+  */    
+}
+
+
